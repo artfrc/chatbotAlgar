5df30fa6841cadbf5b75704187c48c4a48ce616b
Arthur <crfernandesarthur@gmail.com>
2024-06-28 10:27:42 -0300

    Footer quase OK  #2

--- ariana/src/components/layout/Footer.js
+++ ariana/src/components/layout/Footer.js
@@ -1,6 +1,23 @@
+import {FaLinkedin, FaYoutube, FaInstagram} from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer () {
     return(
-        <p>Footer</p>
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li>
+                    <FaYoutube></FaYoutube>
+                </li>
+                <li>
+                    <FaInstagram></FaInstagram>
+                </li><li>
+                    <FaLinkedin></FaLinkedin>
+                </li>
+            </ul>
+        <p className={styles.copy_right}>
+            <span >Ariana</span> &copy; 2024
+        </p>
+        </footer>
     )
 }
 export default  Footer
