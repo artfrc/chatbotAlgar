578379be33a285e7175963cb94e5affa3ce944fc
Arthur <crfernandesarthur@gmail.com>
2024-06-28 11:06:58 -0300

    urls icons footer OK  #2

--- ariana/src/components/layout/Footer.js
+++ ariana/src/components/layout/Footer.js
@@ -6,12 +6,18 @@ function Footer () {
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
                 <li>
-                    <FaYoutube></FaYoutube>
+                    <a href="https://www.youtube.com/@algartelecom" target="_blank">
+                        <FaYoutube />
+                    </a>
                 </li>
                 <li>
-                    <FaInstagram></FaInstagram>
+                    <a href="https://loja.algartelecom.com.br/para-voce/internet/?utm_source=google&utm_medium=cpc&utm_campaign={{campaign.name}}&utm_content={{adset.name}}&utm_term=&gad_source=1&gclid=CjwKCAjwvvmzBhA2EiwAtHVrb39Vf2NiG6gLT7YuX5kcG-gPuU61HMasj8xMeboTep-fR8gD4yDvCxoCc4MQAvD_BwE" target="_blank">
+                        <FaInstagram />
+                    </a>
                 </li><li>
-                    <FaLinkedin></FaLinkedin>
+                    <a href="https://www.linkedin.com/company/algar-telecom/mycompany/" target="_blank">
+                        <FaLinkedin />
+                </a>
                 </li>
             </ul>
         <p className={styles.copy_right}>
