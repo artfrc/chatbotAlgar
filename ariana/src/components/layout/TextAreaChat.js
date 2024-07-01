22996fd50e72b9228fd3ec0feaac46d2b41cac5e
Arthur <crfernandesarthur@gmail.com>
2024-07-01 17:14:27 -0300

    place holder textarea #2

--- ariana/src/components/layout/TextAreaChat.js
+++ ariana/src/components/layout/TextAreaChat.js
@@ -6,7 +6,8 @@ function TextAreaChat() {
     return(
         <div className={styles.container}>
             <div className={styles.textarea_container}>
-                <textarea className={styles.textarea}></textarea>
+                <textarea 
+                className={styles.textarea} placeholder="Digite aqui sua mensagem ou grave um áudio clicando no botão ao lado" ></textarea>
                 <button className={styles.button}>
                     <FaPaperPlane />            
                 </button>
