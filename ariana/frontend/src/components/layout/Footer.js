a0591e22e7af3604cac94bdb83eaf5aaec684d54
Arthur <crfernandesarthur@gmail.com>
2024-07-19 11:00:05 -0300

    tirei os warnings adicionando 'rel='noreferrer' em footer.js

--- ariana/frontend/src/components/layout/Footer.js
+++ ariana/frontend/src/components/layout/Footer.js
@@ -6,16 +6,24 @@ function Footer () {
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
                 <li>
-                    <a href="https://www.youtube.com/@algartelecom" target="_blank">
+                    <a 
+                        href="https://www.youtube.com/@algartelecom" 
+                        target="_blank"
+                        rel='noreferrer'>
                         <FaYoutube />
                     </a>
                 </li>
                 <li>
-                    <a href="https://loja.algartelecom.com.br/para-voce/internet/?utm_source=google&utm_medium=cpc&utm_campaign={{campaign.name}}&utm_content={{adset.name}}&utm_term=&gad_source=1&gclid=CjwKCAjwvvmzBhA2EiwAtHVrb39Vf2NiG6gLT7YuX5kcG-gPuU61HMasj8xMeboTep-fR8gD4yDvCxoCc4MQAvD_BwE" target="_blank">
+                    <a 
+                        href="https://loja.algartelecom.com.br/para-voce/internet/?utm_source=google&utm_medium=cpc&utm_campaign={{campaign.name}}&utm_content={{adset.name}}&utm_term=&gad_source=1&gclid=CjwKCAjwvvmzBhA2EiwAtHVrb39Vf2NiG6gLT7YuX5kcG-gPuU61HMasj8xMeboTep-fR8gD4yDvCxoCc4MQAvD_BwE" target="_blank"
+                        rel='noreferrer'>
                         <FaInstagram />
                     </a>
                 </li><li>
-                    <a href="https://www.linkedin.com/company/algar-telecom/mycompany/" target="_blank">
+                    <a 
+                        href="https://www.linkedin.com/company/algar-telecom/mycompany/" 
+                        target="_blank"
+                        rel='noreferrer'>
                         <FaLinkedin />
                 </a>
                 </li>
